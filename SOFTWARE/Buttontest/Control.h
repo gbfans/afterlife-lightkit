ab82d3c47c3f52d8b358d7f680cc25c1fba44bb8
AJ Quick <ajquick@gmail.com>
2022-07-28 11:00:18 -0600

    Create Control.h

---
+++ SOFTWARE/Buttontest/Control.h
@@ -0,0 +1,27 @@
+#include <Bounce2.h>
+
+#ifndef Control_h
+#define Control_h
+
+enum CONTROL_MODES {
+    STANDALONE,
+    CONTROLLED
+};
+
+class Control {
+    public:
+      void init();
+      void update();
+    private:
+      Bounce2::Button _enableButton;
+      Bounce2::Button _fireButton;
+      Bounce2::Button _ventButton;
+      Bounce2::Button _changeButton;
+      CONTROL_MODES _mode;
+      void _updateControlled();
+      void _updateStandalone();
+      int _controllerMode;
+    
+};
+
+#endif
